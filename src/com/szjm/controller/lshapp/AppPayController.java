a2dfeb1dd661bb7daf8f9d3d6f4b939d0cba5d10
nunu <baiseliunian@outlook.com>
2018-12-05 09:25:49 +0800

    1.新增支付成功 给用户添加金豆(用户累加金豆数量)方法 2.新增支付宝回调地址方法

--- src/com/szjm/controller/lshapp/AppPayController.java
+++ src/com/szjm/controller/lshapp/AppPayController.java
@@ -1,19 +1,25 @@
 package com.szjm.controller.lshapp;
 
-import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.alipay.api.internal.util.AlipaySignature;
 import com.szjm.controller.base.BaseController;
 import com.szjm.service.lsh.agent.AgentManager;
 import com.szjm.service.lsh.agentpurchase.AgentPurchaseManager;
@@ -25,6 +31,7 @@ import com.szjm.util.Jurisdiction;
 import com.szjm.util.PageData;
 import com.szjm.util.Tools;
 
+import spiderman.alipay.config.AlipayConfig;
 import spiderman.alipay.util.AlipayInterfaceInvokeUtil;
 import spiderman.wechat.domain.param.UnifiedOrderSend;
 import spiderman.wechat.domain.result.UnifiedOrderResult;
@@ -40,16 +47,23 @@ import spiderman.wechat.util.SignUtil;
 @RequestMapping(value = "/lshapp/pay")
 public class AppPayController extends BaseController {
 
+	public Logger log = (Logger) LoggerFactory.getLogger(AppPayController.class);
+	
 	@Resource(name = "agentpurchaseService")
 	private AgentPurchaseManager agentpurchaseService;
+	
 	@Resource(name = "beanrechargeService")
 	private BeanRechargeManager rechargeService;
+	
 	@Resource(name = "userService")
 	private UserManager userService;
+	
 	@Resource(name = "beanService")
 	private BeanManager beanService;
+	
 	@Resource(name = "agentService")
 	private AgentManager agentService;
+	
 	@Resource(name = "appuserService")
 	private AppuserManager appuserService;
 	
@@ -57,33 +71,34 @@ public class AppPayController extends BaseController {
 	 * 购买金豆
 	 * @param userId 用户id
 	 * @param agentId 代理配置id
+	 * @param agentStatus 代理状态（0.会员，1.经销商，2.企商VIP）
+	 * 
 	 * @param beanId 金豆充值id
-	 * @param payment 支付方式（0.微信）
-	 * @param money 支付金额（1.支付宝）
+	 * @param payment 支付方式（0.微信,1.支付宝）
+	 * @param totalAmount 支付金额
 	 * @throws Exception
 	 */
 	@Transactional
 	@RequestMapping(value = "/buyGold")
 	@ResponseBody
-	public Object buyGold(String userId, String agentId,String beanId, String payment, Double money,String ipAddress) throws Exception {
-		
+	public Object buyGold(String userId, String agentId,String beanId, /*String agentStatus,*/
+			String payment, String totalAmount,String ipAddress) throws Exception {
 		Map<String, Object> aliResult = new HashMap<String, Object>();
 		String number ="";
-		if (payment.equals("0")) {
-			String type = "0";
+		if (payment.equals("0")) {//微信支付
 			//微信
 			if (null != agentId && !agentId.equals("")) {
 				//代理购买
-				number = purchaseAdd(userId,  agentId,  money, type);
+				number = purchaseAdd(userId,  agentId,  /*agentStatus,*/ totalAmount, payment);
 			}else if (null != beanId && !beanId.equals("")) {
 				//金豆充值
-				number = rechargeAdd(userId,  beanId,  money, type);
+				number = rechargeAdd(userId,  beanId,  totalAmount, payment);
 			}
 			UnifiedOrderSend unifiedOrderSend = new UnifiedOrderSend();
 			// send.set
 			unifiedOrderSend.setBody("礼尚汇商品");
 			unifiedOrderSend.setOut_trade_no(number);
-			Double realPay = Double.valueOf(money) * 100;
+			Double realPay = Double.valueOf(totalAmount) * 100;
 			String real_pay = realPay.toString();
 			String fee = real_pay;
 			if (fee.contains(".")) {
@@ -99,21 +114,20 @@ public class AppPayController extends BaseController {
 			wxResult.put("paytype", "wxpay");
 			wxResult.put("order_id", number);
 			
-		} else if(payment.equals("1")) {
-			String type = "1";
+		} else if(payment.equals("1")) {//支付宝支付
 			//支付宝
 			if (null != agentId && !agentId.equals("")) {
 				//代理购买
-				number = purchaseAdd(userId,  agentId,  money, type);
+				number = purchaseAdd(userId,  agentId, /*agentStatus,*/ totalAmount, payment);
 			}else if (null != beanId && !beanId.equals("")) {
 				//金豆充值
-				number = rechargeAdd(userId,  beanId,  money, type);
+				number = rechargeAdd(userId,  beanId,  totalAmount, payment);
 			}
 			aliResult.put("paytype", "alipay");
 			/*String alipayMessge = AlipayInterfaceInvokeUtil.AlipayTradeAppPay(
 					"礼尚汇商品", "礼尚汇支付", out_order_id, real_pay);*/
 			String outtradeno = number;
-			String totalAmount = "0.01";
+			//String totalAmount = "0.01";
 			String alipayMessge = AlipayInterfaceInvokeUtil.AlipayTradeAppPay(
 					"礼尚汇商品", "礼尚汇支付", outtradeno, totalAmount);
 			aliResult.put("alipaymessge", alipayMessge
@@ -123,14 +137,15 @@ public class AppPayController extends BaseController {
 		return aliResult;
 	}
 	
-
 	/**
 	 * 新增代理购买记录
-	 * 
-	 * @param page
+	 * @param userId 用户id
+	 * @param agentId 代理配置id
+	 * @param payment 支付方式（0.微信,1.支付宝）
+	 * @param totalAmount 支付金额
 	 * @throws Exception
 	 */
-	public String purchaseAdd(String userId, String agentId, Double money,String type) throws Exception {
+	public String purchaseAdd(String userId, String agentId, /*String agentStatus,*/String totalAmount,String payment) throws Exception {
 		Integer appUserId = Jurisdiction.getAppUserId();
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
@@ -145,12 +160,12 @@ public class AppPayController extends BaseController {
 		PageData agent = new PageData();
 		agent.put("AGENT_ID", agentId);
 		agent = agentService.findById(agent);
-		pd.put("AGENT_STATUS", agent.get("STATUS")); // 代理状态
-		pd.put("AGENT_PRICE", money); // 支付金额
-		pd.put("TYPE", type); // 支付方式
+		pd.put("AGENT_STATUS", agent.get("STATUS")); // 代理状态（0.会员，1.经销商，2.企商VIP）
+		//pd.put("AGENT_STATUS", agentStatus); // 代理状态（0.会员，1.经销商，2.企商VIP）
+		pd.put("AGENT_PRICE", totalAmount); // 支付金额
+		pd.put("TYPE", payment); // 支付方式（0.微信，1.支付宝）
 		pd.put("CREATE_TIME", Tools.date2Str(new Date())); // 创建时间
-		pd.put("STATUS", "0"); // 创建时间
-		pd.put("PAY_TIME", ""); // 创建时间
+		pd.put("STATUS", "0"); // 支付状态（0.未支付，1.已支付）
 		agentpurchaseService.save(pd);
 		return number;
 	}
@@ -158,10 +173,13 @@ public class AppPayController extends BaseController {
 	/**
 	 * 新增金豆充值记录
 	 * 
-	 * @param page
+	 * @param userId 用户id
+	 * @param beanId 金豆充值id
+	 * @param payment 支付方式（0.微信,1.支付宝）
+	 * @param totalAmount 支付金额
 	 * @throws Exception
 	 */
-	public String rechargeAdd(String userId, String beanId, Double money,String type) throws Exception {
+	public String rechargeAdd(String userId, String beanId, String totalAmount,String payment) throws Exception {
 		Integer appUserId = Jurisdiction.getAppUserId();
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
@@ -177,12 +195,11 @@ public class AppPayController extends BaseController {
 		bean.put("BEAN_ID", beanId);
 		bean = beanService.findById(bean);
 		pd.put("BEAN_NUMBER", bean.get("BEAN_NUMBER")); // 充值数量
-		pd.put("BEAN_PRICE", money); // 支付金额
-		pd.put("TYPE", type); // 支付方式
+		pd.put("BEAN_PRICE", totalAmount); // 支付金额
+		pd.put("TYPE", payment); // 支付方式
 		pd.put("CREATE_TIME", Tools.date2Str(new Date())); // 创建时间
-		pd.put("STATUS", "0");  
-		pd.put("PAY_TIME", ""); 
-		agentpurchaseService.save(pd);
+		pd.put("STATUS", "0");  //支付状态（0.未支付，1.已支付）
+		rechargeService.save(pd);
 		
 		//给用户添加金豆
 		/*PageData user = new PageData();
@@ -194,18 +211,89 @@ public class AppPayController extends BaseController {
 		appuserService.saveU(user);*/
 		return number;
 	}
-	
+
 	/**
-	 * 支付成功 给用户添加金豆(用户累加金豆数量)
-	 * @param rechargeId
+	 * 支付宝回调地址
+	 * 
+	 * @param request
+	 * @param httpResponse
+	 * @param session
 	 * @return
 	 * @throws Exception
 	 */
-	public String lshAppUserUpdate(String rechargeId) throws Exception {
+	@RequestMapping(value = "/alipayReturnOrder")
+	public void alipayReturnOrder(HttpServletRequest request, HttpServletResponse httpResponse, HttpSession session)
+			throws Exception {
+		// 获取支付宝POST过来反馈信息
+		Map<String, String> params = new HashMap<String, String>();
+		Map requestParams = request.getParameterMap();
+		Iterator iter = requestParams.keySet().iterator();
+		while (iter.hasNext()) {
+			String name = (String) iter.next();
+			String[] values = (String[]) requestParams.get(name);
+			String valueStr = "";
+			for (int i = 0; i < values.length; i++) {
+				valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+			}
+			// 乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+			// valueStr = new String(valueStr.getBytes("ISO-8859-1"), "gbk");
+			params.put(name, valueStr);
+		}
+
+		// 获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+		// 商户订单号
+		String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+		// 支付宝交易号
+		String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"), "UTF-8");
+		// 交易状态
+		String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"), "UTF-8");
+		// 支付金额
+		String total_amount = new String(request.getParameter("total_amount").getBytes("ISO-8859-1"), "UTF-8");
+		// 获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表
+		boolean verify_result = AlipaySignature.rsaCheckV1(params, AlipayConfig.ALIPAY_PUBLIC_KEY, AlipayConfig.CHARSET,
+				AlipayConfig.SIGNTYPE);
+
+		if (verify_result) {// 验证成功
+			if (trade_status.equals("TRADE_SUCCESS")) {// 支付成功
+				synchronized (out_trade_no.intern()) {
+					PageData pd = new PageData();
+					pd.put("PURCHASE_NUMBER", out_trade_no);//购买编号
+					PageData  lshAgentPurchase = agentpurchaseService.findPurchaseNumber(pd);
+					if(lshAgentPurchase != null) {//如果lshAgentPurchase不等于null进行购买代理支付 否则金豆充值支付
+						String purchaseId = "";
+						//支付成功后调用修改的方法
+						purshaseUpdate(purchaseId);
+					}else if(lshAgentPurchase == null) {//金豆充值
+						pd.put("RECHARGE_NUMBER", out_trade_no);//充值编号
+						PageData lshBeanRecharge = rechargeService.findRechargeNumber(pd);
+						if(lshBeanRecharge != null) {
+							//支付成功后调用修改的方法(用户累加金豆数量)
+							String userId = "";
+							lshAppUserUpdate(userId);
+							//支付成功后如果是金豆充值就调用以下方法
+							String rechargeId ="";
+							rechargeUpdate(rechargeId);
+						}
+					}
+				}
+			}
+		} else {
+			// 验证失败
+			log.info("验证失败!");
+		}
+	}
+	
+	/**
+	 * 支付成功 给用户添加金豆(用户累加金豆数量)
+	 * @param userId 用户id
+	 * @return
+	 * @throws Exception
+	 */
+	public String lshAppUserUpdate(String userId) throws Exception {
 		// 根据金豆配置id查询配置信息
 		PageData bean = new PageData();
 		PageData user = new PageData();
-		user.put("USER_ID", user);
+		user.put("USER_ID", userId);
 		user = appuserService.findByUiId(user);
 		Integer  integration = Integer.parseInt(user.get("INTEGRATION").toString());
 		Integer  bean_number = Integer.parseInt(bean.get("BEAN_NUMBER").toString());
@@ -216,12 +304,11 @@ public class AppPayController extends BaseController {
 
 	/**
 	 * 支付成功后修改金豆充值记录
-	 * @param beanId
+	 * @param rechargeId 金豆充值id
 	 * @return
 	 * @throws Exception
 	 */
 	public String rechargeUpdate(String rechargeId) throws Exception {
-		
 		PageData pd = new PageData();
 		pd.put("RECHARGE_ID", rechargeId);
 		PageData recharge = rechargeService.findById(pd);
@@ -233,12 +320,11 @@ public class AppPayController extends BaseController {
 	
 	/**
 	 * 支付成功后修改代理购买记录
-	 * @param beanId
+	 * @param purchaseId 代理购买id
 	 * @return
 	 * @throws Exception
 	 */
 	public String purshaseUpdate(String purchaseId) throws Exception {
-		
 		PageData pd = new PageData();
 		pd.put("PURCHASE_ID", purchaseId);
 		PageData purchase = agentpurchaseService.findById(pd);
@@ -247,6 +333,8 @@ public class AppPayController extends BaseController {
 		agentpurchaseService.edit(purchase);
 		return "修改成功";
 	}
+	
+	
 	
 	/**
 	 * 购买金豆
@@ -257,7 +345,7 @@ public class AppPayController extends BaseController {
 	 * @param money 支付金额（1.支付宝）
 	 * @throws Exception
 	 */
-	@Transactional
+	/*@Transactional
 	@RequestMapping(value = "/buyGift")
 	@ResponseBody
 	public Object pay(String userId, String agentId,String beanId, String payment, Double money,String ipAddress) throws Exception {
@@ -305,8 +393,8 @@ public class AppPayController extends BaseController {
 				number = rechargeAdd(userId,  beanId,  money, type);
 			}
 			aliResult.put("paytype", "alipay");
-			/*String alipayMessge = AlipayInterfaceInvokeUtil.AlipayTradeAppPay(
-					"礼尚汇商品", "礼尚汇支付", out_order_id, real_pay);*/
+			String alipayMessge = AlipayInterfaceInvokeUtil.AlipayTradeAppPay(
+					"礼尚汇商品", "礼尚汇支付", out_order_id, real_pay);
 			String outtradeno = number;
 			String totalAmount = "0.01";
 			String alipayMessge = AlipayInterfaceInvokeUtil.AlipayTradeAppPay(
@@ -316,5 +404,5 @@ public class AppPayController extends BaseController {
 			aliResult.put("order_id", number);
 		}
 		return aliResult;
-	}
+	}*/
 }
